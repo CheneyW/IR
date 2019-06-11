8634f0bde2a85e4c565a2e6cb2806929e6584ad8
Cheney <wcy708708@gmail.com>
2019-06-11 15:45:12 +0800

    svm rank

---
+++ lab2/answer_sentence_selection/svm_rank/split.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author : 王晨懿
+@studentID : 1162100102
+@time : 2019/5/10
+"""
+import os
+
+split_idx = 4500
+
+
+def split():
+    train = []
+    test = []
+    with open('svmrank_train.dat', 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            qid = line.split()[1][4:]
+            if int(qid) < split_idx:
+                train.append(line)
+            else:
+                test.append(line)
+
+    with open('train.dat', 'w', encoding='utf-8')as f:
+        f.writelines(train)
+
+    with open('test.dat', 'w', encoding='utf-8')as f:
+        f.writelines(test)
+
+
+if __name__ == '__main__':
+    split()
+
+    os.system('svm_rank_learn.exe -c 200 train.dat model')
+
+    os.system('svm_rank_classify.exe test.dat model predictions.dat')
+
+# 0.7333559480743967 0.7406188458548961
